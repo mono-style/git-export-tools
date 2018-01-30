15c8669936e4cd08d49b70dfb3c141551b1c0528
miura <miura@mono-style.co.jp>
2018-01-30 10:21:30 +0900

    tag があるとうまく動かなかったので該当箇所コメントアウト

--- git-export-diff.rb
+++ git-export-diff.rb
@@ -77,8 +77,8 @@ else
   # get tag name or branch name
   desc = %x[git describe --contains --all #{sha}]
   name = case desc
-    when /^tags/
-      desc[/tags\/([\w\/]+)\^0/, 1].strip
+    # when /^tags/
+    #   desc[/tags\/([\w\/]+)\^0/, 1].strip
     when /^remotes/
       desc[/remotes\/(\w+\/\w+)/, 1].gsub("/", "-").strip + "-#{sha[0,7]}"
     else
